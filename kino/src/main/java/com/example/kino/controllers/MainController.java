d791f5a7bedf0c810c98fbf8e6d30c352bc50988
nastysik441l <122270884+nastysik441l@users.noreply.github.com>
2024-03-23 16:51:13 +0200

    п

--- kino/src/main/java/com/example/kino/controllers/MainController.java
+++ kino/src/main/java/com/example/kino/controllers/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+
     @Autowired
     private PostRepository postRepository;
 
@@ -17,12 +18,37 @@ public class MainController {
     public String menuMain(Model model){
         Iterable<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
+
         return "menu-main";
     }
     @GetMapping("/cinema")
     public String cinemas(Model model) {
         model.addAttribute("title", "Кинотеатры");
         return "cinema";
+    }
+    @GetMapping("/konsol")
+    public String konsols(Model model) {
+        model.addAttribute("title", "Консультация");
+        return "konsol";
+    }
+    @GetMapping("/texnik")
+    public String texniks(Model model) {
+        model.addAttribute("title", "Акции и скидки");
+        return "texnik";
+    }  @GetMapping("/delete")
+    public String delete(Model model) {
+        model.addAttribute("title", "Удаление аккаунта");
+        return "delete";
+    }
+    @GetMapping("/action")
+    public String actions(Model model) {
+        model.addAttribute("title", "Технические сложности");
+        return "action";
+    }
+    @GetMapping("/action2")
+    public String actionfs(Model model) {
+        model.addAttribute("title", "Акции и скидки");
+        return "action2";
     }
     @GetMapping("/theaters/add")
     public String theatersAdd(Model model){
